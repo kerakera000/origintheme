9767f180b1d423f1ac3a2adbb0056e73505ae18b
Akira Katakura <akira.katakura.01@gmail.com>
2025-04-02 03:34:15 +0900

    CareerのPheroを編集(html,scss,img)

--- page-career.php
+++ page-career.php
@@ -17,8 +17,8 @@ get_template_part('./template/head');
         <div class="Phero--cont inner-content1400">
             <div class="Phero--textbox">
                 <h2 class="common-titlebox">
-                    <span class="title">About Us</span>
-                    <span class="sub-title">COMPANY事業部について</span>
+                    <span class="title">Career</span>
+                    <span class="sub-title">キャリア・研修について</span>
                 </h2>
                 <div class="Phero--textbox--list">
                     <p class="index">Index</p>
@@ -28,7 +28,7 @@ get_template_part('./template/head');
                                 <span class="link--number">
                                     <img src="<?php echo get_template_directory_uri(); ?>/assets/img/Pabout/Polygon.svg" alt="">
                                 </span>
-                                <span class="link--text">ミッション</span>
+                                <span class="link--text">職種紹介</span>
                             </a>
                         </li>
                         <li class="list--item">
@@ -36,7 +36,7 @@ get_template_part('./template/head');
                                 <span class="link--number">
                                     <img src="<?php echo get_template_directory_uri(); ?>/assets/img/Pabout/Polygon.svg" alt="">
                                 </span>
-                                <span class="link--text">ミッション</span>
+                                <span class="link--text">キャリアパス</span>
                             </a>
                         </li>
                         <li class="list--item">
@@ -44,7 +44,7 @@ get_template_part('./template/head');
                                 <span class="link--number">
                                     <img src="<?php echo get_template_directory_uri(); ?>/assets/img/Pabout/Polygon.svg" alt="">
                                 </span>
-                                <span class="link--text">ミッション</span>
+                                <span class="link--text">研修プログラム</span>
                             </a>
                         </li>
                         <li class="list--item">
@@ -52,22 +52,26 @@ get_template_part('./template/head');
                                 <span class="link--number">
                                     <img src="<?php echo get_template_directory_uri(); ?>/assets/img/Pabout/Polygon.svg" alt="">
                                 </span>
-                                <span class="link--text">ミッション</span>
+                                <span class="link--text">スキルアップ支援</span>
                             </a>
                         </li>
                     </ul>
                 </div>
             </div>
             <div class="Phero--imgbox">
-                <p class="breadcrumbs">Top - About Us</p>
-                <img class="hero-img" src="<?php echo get_template_directory_uri(); ?>/assets/img/top/aboutimg.png" alt="">
+                <p class="breadcrumbs">Top - Career</p>
+                <img class="hero-img" src="<?php echo get_template_directory_uri(); ?>/assets/img/top/careerimg2.png" alt="">
             </div>
         </div>
     </div>
     <div class="PheroText">
         <p class="text inner-content1400">
             <span class="text--content">
-                セラクのCOMPANY事業部では、総合人事システムCOMPANY(R)の新規導入から運用に至るまで、幅広いサポートを提供しています。属人化の撤廃や工数削減、業務効率化など、お客さまが抱える人事業務の課題を解決し、事業成長を力強く後押しします。私たちは数々の大手企業から信頼をいただき、社会の基盤を支えるパートナーとしての役割を果たしています。
+            お客さまの課題を解消し、事業成長に貢献するために、COMPANY事業部は職<br>
+            種の垣根を超えて連携しています。職種はエンジニア職とビジネス職に大別<br>
+            され、キャリア形成も多種多様です。その人材の厚みが事業部の推進力につな<br>
+            がっています。それぞれの社員はさまざまなバックグラウンドをもっており、<br>
+            業務一つひとつに真摯に向き合っています。<br>
             </span>
         </p>
     </div>
