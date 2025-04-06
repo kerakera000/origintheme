e9cb6b0b0dd7a843c03a54fc2aa1d7fbbc58e760
SasshaAmiri <sashimiofficial7@gmail.com>
2025-04-06 23:42:00 +0900

    Sassha-> carrer (1)

--- page-career.php
+++ page-career.php
@@ -83,8 +83,11 @@ get_template_part('./template/head');
             </div>
             <div class="PCbusiness--type-box">
                 <div class="category-box">
-                    <h2 class="category-box--title">エンジニア職</h2>
-                    <img class="category-box--img" src="<?php echo get_template_directory_uri(); ?>/assets/img/Pbusiness/仮 300-380.JPG" alt="">
+                    <h2 class="category-box--title">
+                        カスタマーサクセス/<br>
+                        エンジニア職
+                    </h2>
+                    <img class="category-box--img" src="<?php echo get_template_directory_uri(); ?>/assets/img/Pbusiness/Rectangle72.png" alt="">
                 </div>
                 <div class="introduction-box">
                     <div class="job-box">
@@ -92,60 +95,39 @@ get_template_part('./template/head');
                             <div class="num-box">
                                 <span class="job-box-num">(01)</span>
                                 <div class="textbox">
-                                    <span class="textbox--jobtitle">管理</span>
+                                    <span class="textbox--jobtitle">COMPANYエンジニア</span>
                                     <p class="textbox--text">
-                                        自社の目標達成に向けた組織運営に携わります。管轄する領域は幅広く、一般課員の監督や人材の<br>
-                                        採用・評価、予算管理など、組織の中核をなす人材です。技術的なスキルはもちろんのこと、物ごと<br>
-                                        の本質を見抜くコンセプチュアルスキル、他者と良好な関係を築くためのヒューマンスキルなど<br>
-                                        が求められます。<br>
+                                        お客さまが抱えている課題や要望などを踏まえた要件定義に基づいて、COMPANY(R)︎のシステム<br>
+                                        を構築します。担当する領域は人事・給与・勤怠・WEBに分かれており、導入支援や機能追加などの<br>
+                                        プロジェクトに参画します。IT業界未経験からスタートしたエンジニアも多く、技術研修や日々の<br>
+                                        業務などを通してスキルアップに努めています。<br>
                                     </p>
                                 </div>
                             </div>
+                            <hr class="hr1">
                         </div>
-                        <span class="partition-line">
-                            <img src="<?php echo get_template_directory_uri(); ?>/assets/img/Pcareer/line1.svg" alt="">
-                        </span>
                         <div class="job-box--content">
                             <div class="num-box">
-                                <span class="job-box-num">(01)</span>
+                                <span class="job-box-num">(02)</span>
                                 <div class="textbox">
-                                    <span class="textbox--jobtitle">管理</span>
+                                    <span class="textbox--jobtitle">IF / インターフェースエンジニア</span>
                                     <p class="textbox--text">
-                                        自社の目標達成に向けた組織運営に携わります。管轄する領域は幅広く、一般課員の監督や人材の<br>
-                                        採用・評価、予算管理など、組織の中核をなす人材です。技術的なスキルはもちろんのこと、物ごと<br>
-                                        の本質を見抜くコンセプチュアルスキル、他者と良好な関係を築くためのヒューマンスキルなど<br>
-                                        が求められます。<br>
+                                        COMPANY(R)の新規導入・運用/保守において、人事基幹システムとして他システムとの外部連携、<br>
+                                        要件定義からデータ連携（IN/OUT）業務を担当します。お客さまと直接やりとりする機会も多く、<br>
+                                        コミュニケーション力や提案力を活かすことができますし、さらにIF領域でのコードを使用した<br>
+                                        開発に携わることでより専門的なスキルを身につけることが可能です。<br>
                                     </p>
                                 </div>
                             </div>
+                            <hr class="hr1">
                         </div>
-                        <span class="partition-line">
-                            <img src="<?php echo get_template_directory_uri(); ?>/assets/img/Pcareer/line1.svg" alt="">
-                        </span>
-                        <div class="job-box--content">
-                            <div class="num-box">
-                                <span class="job-box-num">(01)</span>
-                                <div class="textbox">
-                                    <span class="textbox--jobtitle">管理</span>
-                                    <p class="textbox--text">
-                                        自社の目標達成に向けた組織運営に携わります。管轄する領域は幅広く、一般課員の監督や人材の<br>
-                                        採用・評価、予算管理など、組織の中核をなす人材です。技術的なスキルはもちろんのこと、物ごと<br>
-                                        の本質を見抜くコンセプチュアルスキル、他者と良好な関係を築くためのヒューマンスキルなど<br>
-                                        が求められます。<br>
-                                    </p>
-                                </div>
-                            </div>
-                        </div>
-                        <span class="partition-line">
-                            <img src="<?php echo get_template_directory_uri(); ?>/assets/img/Pcareer/line1.svg" alt="">
-                        </span>
                     </div>
                 </div>
             </div>
             <div class="PCbusiness--type-box">
                 <div class="category-box">
-                    <h2 class="category-box--title">エンジニア職</h2>
-                    <img class="category-box--img" src="<?php echo get_template_directory_uri(); ?>/assets/img/Pbusiness/仮 300-380.JPG" alt="">
+                    <h2 class="category-box--title">ビジネス職</h2>
+                    <img class="category-box--img" src="<?php echo get_template_directory_uri(); ?>/assets/img/Pbusiness/Rectangle73.png" alt="">
                 </div>
                 <div class="introduction-box">
                     <div class="job-box">
@@ -162,71 +144,61 @@ get_template_part('./template/head');
                                     </p>
                                 </div>
                             </div>
+                            <hr class="hr1">
                         </div>
-                        <span class="partition-line">
-                            <img src="<?php echo get_template_directory_uri(); ?>/assets/img/Pcareer/line1.svg" alt="">
-                        </span>
                         <div class="job-box--content">
                             <div class="num-box">
-                                <span class="job-box-num">(01)</span>
+                                <span class="job-box-num">(02)</span>
                                 <div class="textbox">
-                                    <span class="textbox--jobtitle">管理</span>
+                                    <span class="textbox--jobtitle">採用</span>
                                     <p class="textbox--text">
-                                        自社の目標達成に向けた組織運営に携わります。管轄する領域は幅広く、一般課員の監督や人材の<br>
-                                        採用・評価、予算管理など、組織の中核をなす人材です。技術的なスキルはもちろんのこと、物ごと<br>
-                                        の本質を見抜くコンセプチュアルスキル、他者と良好な関係を築くためのヒューマンスキルなど<br>
-                                        が求められます。<br>
+                                        優秀な人材を獲得するための採用戦略立案や、人材紹介会社との折衝など、主に人事採用にまつわ<br>
+                                        る業務にあたります。また、新入社員を受け入れたあとの入社手続きも大切な業務のひとつです。採<br>
+                                        用面接の面接官を担当することもあり、自社とマッチする人材を見極める力が磨かれます。<br>
                                     </p>
                                 </div>
                             </div>
+                            <hr class="hr1">
                         </div>
-                        <span class="partition-line">
-                            <img src="<?php echo get_template_directory_uri(); ?>/assets/img/Pcareer/line1.svg" alt="">
-                        </span>
                         <div class="job-box--content">
                             <div class="num-box">
-                                <span class="job-box-num">(01)</span>
+                                <span class="job-box-num">(03)</span>
                                 <div class="textbox">
-                                    <span class="textbox--jobtitle">管理</span>
+                                    <span class="textbox--jobtitle">教育</span>
                                     <p class="textbox--text">
-                                        自社の目標達成に向けた組織運営に携わります。管轄する領域は幅広く、一般課員の監督や人材の<br>
-                                        採用・評価、予算管理など、組織の中核をなす人材です。技術的なスキルはもちろんのこと、物ごと<br>
-                                        の本質を見抜くコンセプチュアルスキル、他者と良好な関係を築くためのヒューマンスキルなど<br>
-                                        が求められます。<br>
+                                        セラクは充実した教育制度を設けており、IT業界未経験でもエンジニアとして必要なスキルを習<br>
+                                        得できます。こうした育成環境を整備するのが教育部門の担当者です。新入社員の基礎教育やさら<br>
+                                        なる成長を望む社員の継続教育など、各プログラムは業界の動向に合わせて、日々アップデートし<br>
+                                        ていきます。<br>
                                     </p>
                                 </div>
                             </div>
+                            <hr class="hr1">
                         </div>
-                        <span class="partition-line">
-                            <img src="<?php echo get_template_directory_uri(); ?>/assets/img/Pcareer/line1.svg" alt="">
-                        </span>
                     </div>
                 </div>
             </div>
-
         </div>
     </div>
+
     <div class="PCcareer-path">
-        <div class="PCcareer-path--content inner-content1400">
+        <div class="PCcareer-path--content inner-content">
             <div class="common-titlebox left">
                 <span class="sub-title">Career Path</span>
                 <h2 class="title">キャリアパス</h2>
             </div>
-            <div class="text">
-                <p>
-                    COMPANY事業部のキャリアパスは大きく分けて3つあります。マネジメント・スペシャリスト・組織運営
-                    と、社員一人ひとりがそれぞれの特性を活かし、能力を発揮できるキャリアを進んでいくことができます。
-                </p>
+            <p class="PCcareer-path--text">COMPANY事業部のキャリアパスは大きく分けて3つあります。マネジメント・スペシャリスト・組織運営<br>
+                と、社員一人ひとりがそれぞれの特性を活かし、能力を発揮できるキャリアを進んでいくことができます。
+            </p>
+            <div class="PCcareer-path--imgbox">
+                <img src="<?php echo get_template_directory_uri(); ?>/assets/img/Pcareer/Group152.svg" alt="">
+            </div>
+            <div class="PCcareer-path--cardbox">
+                <img src="<?php echo get_template_directory_uri(); ?>/assets/img/Pcareer/Group155.svg" alt="">
             </div>
         </div>
-            
-        <div class="PCcareer-path--imgbox">
-            <img src="<?php echo get_template_directory_uri(); ?>/assets/img/Pcareer/CP chart.svg" alt="">
-        </div>
-        <div class="PCcareer-path--cardbox">
-            <img src="<?php echo get_template_directory_uri(); ?>/assets/img/Pcareer/CP Card.svg" alt="">
-        </div>
     </div>
+
     <div class="PCtraining">
         <div class="PCtraining--content inner-content1400">
             <div class="common-titlebox left">
