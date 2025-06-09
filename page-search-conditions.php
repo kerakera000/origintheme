b144c0e08304a98d174e1cbea91c2c5646864f26
A-Katakura <akira.katakura.01@gmail.com>
2025-06-09 16:14:06 +0900

    詳細検索ページのpc版とsp版を作成

--- page-search-conditions.php
+++ page-search-conditions.php
@@ -18,6 +18,7 @@ get_template_part('./template/head');
 ?>
 
 <?php get_template_part('./template/header');?>
+<?php get_template_part('./template/sidebar');?>
 
 <main class="max-content search-conditions-page">
     <?php get_template_part('./template/content-header'); ?>
@@ -33,7 +34,7 @@ get_template_part('./template/head');
                 <div class="search-mainbox__search-box">
                     <div class="flexbox-list">
                         <div class="js-accordion-toggle">
-                            <span class="text">業種から探す</span>
+                            <span class="text">エリアから探す</span>
                             <span class="icon">
                                 <img src="<?php echo get_template_directory_uri(); ?>/assets/img/common-icon/arrow-accordion-pink.svg" alt="">
                             </span>
@@ -60,6 +61,59 @@ get_template_part('./template/head');
                             <button class="common-button sc-button">301万円〜500万円(100件)</button>
                             <button class="common-button sc-button">501万円〜1000万円(100件)</button>
                             <button class="common-button sc-button">1001万円以上(100件)</button>
+                        </div>
+
+                        <span class="addjust-line"></span>
+
+                        <div class="list-title">
+                            <span class="text">開催日で探す</span>
+                        </div>
+                        
+                        <div class=flexbox-list__date-search>
+
+                            <div class=options>
+                                <div class="common-search-accordion-content">
+                                    <!-- ボタン -->
+                                    <button class="button s-button">
+                                        <span class="text s-button__text">開始</span>
+                                        <img class="icon" src="<?php echo get_template_directory_uri(); ?>/assets/img/common-icon/acordion-under-pink.svg" alt="">
+                                    </button>
+                                    <!-- アコーディオン -->
+                                    <div class="content">
+                                        <!-- ここにアコーディオンで表示したい内容を入れる -->
+                                        <ul>
+                                            <li>１月１日</li>
+                                            <li>ー</li>
+                                            <li>１２月３１日</li>
+                                        </ul>
+                                    </div>
+                                </div>
+
+                                <span class="bt-line"></span>
+
+                                <div class="common-search-accordion-content">
+                                    <!-- ボタン -->
+                                    <button class="button s-button">
+                                        <span class="text s-button__text">終了</span>
+                                        <img class="icon" src="<?php echo get_template_directory_uri(); ?>/assets/img/common-icon/acordion-under-pink.svg" alt="">
+                                    </button>
+                                    <!-- アコーディオン -->
+                                    <div class="content">
+                                        <!-- ここにアコーディオンで表示したい内容を入れる -->
+                                        <ul>
+                                            <li>１月１日</li>
+                                            <li>ー</li>
+                                            <li>１２月３１日</li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="front-attention-box__button common-center-button-box">
+                                <button class="common-button-pink pink-button">
+                                    指定した条件で検索
+                                </button>
+                            </div>
                         </div>
                       
                     </div>
