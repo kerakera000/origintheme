f47cfa61d7c5087419755af1d74d34e68865a7f7
kerakera000 <ropesumqaku514@gmail.com>
2025-06-11 04:18:57 +0900

    スワイパー追加

--- assets/js/common.js
+++ assets/js/common.js
@@ -31,5 +31,41 @@ const mySwiper = new Swiper('#SwiperNewItem', {
       },
     }
 });
+
+const frontSwiper = new Swiper('#frontSwiper', {
+  // Optional parameters
+  loop: true,
+  slidesPerView: 1.2,
+  spaceBetween: 24,
+  centeredSlides: true,
+  // speed: 1000,
+  // autoplay: {
+  //   delay: 2500,
+  //   disableOnInteraction: false,
+  // },
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+ 
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+ 
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+  // レスポンシブ設定
+  breakpoints: {
+    // 600px未満の場合
+    768: {
+      slidesPerView: 1.5,
+      spaceBetween: 24
+    },
+  }
+});
 window.addEventListener('load', () => {
 });
